b8ff6035a39c9291bc38799cc168341c0e8a7ba2
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2022-01-29 14:28:07 +0800

    Added progress bar

---
+++ static/js/progress-bar.js
@@ -0,0 +1,14 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("scroll-progress-bar").style.width = scrolled + "%";
+  //   document.getElementById("progress-bar").aria.valuenow = scrolled;
+}
